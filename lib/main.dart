3541ebeb99d954cbf8792806ef56530a3b1259b2
Khalid War <git@khalidwar.com>
2020-10-25 09:52:28 -0500

    Updated Packages

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
       title: 'Banking',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-//        primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
         fontFamily: 'Nunito',
       ),
